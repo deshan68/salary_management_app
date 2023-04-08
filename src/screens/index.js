11d79a180ee8bb567b561a0f678f04785c37fa58
deshan68 <arundeshan@gmail.com>
2023-04-08 18:51:56 +0530

    creat index file for all screens

---
+++ src/screens/index.js
@@ -0,0 +1,6 @@
+export { default as Home } from "../screens/Home";
+export { default as Goal } from "../screens/Goal";
+export { default as AddIncome } from "../screens/AddIncome";
+export { default as AddSpending } from "../screens/AddSpending";
+export { default as CreateGoal } from "../screens/CreateGoal";
+export { default as Savings } from "../screens/Savings";
